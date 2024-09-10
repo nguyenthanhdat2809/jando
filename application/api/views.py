c5e042b884bf39d6ace555f3b69ecf5fa95bee43
dunghx <tienvv@reactplus.com>
2024-09-10 14:01:46 +0700

    [BASE]: initial

---
+++ application/api/views.py
@@ -0,0 +1,8 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HelloWorldAPIView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, World!"}, status=status.HTTP_200_OK)
